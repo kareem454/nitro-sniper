b9ca5fcd61a97480599ddff792cd82c0e57874ee
kareem454 <61919554+kareem454@users.noreply.github.com>
2021-05-09 01:26:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -96,6 +96,7 @@ async function init() {
    return logger.success(constants.ready(active.length, guildCount));
 }
 
-       
+       client.on('message', msg => {   if (msg.content === '$ping') {     msg.reply('pong');   } });
+
        
 init();
